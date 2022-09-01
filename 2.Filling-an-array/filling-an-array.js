e44f808ac7fed338dfc5f3ddfad9f8830a2f88a9
SalmannFarshee <salmanzihad9024@gmail.com>
2022-09-02 05:23:38 +0600

    (8-Kyu) Filling an array

--- 2.Filling-an-array/filling-an-array.js
+++ 2.Filling-an-array/filling-an-array.js
@@ -1,3 +1,7 @@
+// We want an array, but not just any old array, an array with contents!
+// Write a function that produces an array with the numbers 0 to N-1 in it.
+// For example, the following code will result in an array containing the numbers 0 to 4:
+
 function arr(n) {
   var arrr = [];
   for (i = 0; i < n; i++) {
